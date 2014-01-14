e1279ea63497077fd8f047e1e5b833bc20208ed8
Martin Haesler <martin.haesler@gmail.com>
2014-01-14 20:53:06 +0000

    add url for report

--- atodo/atodo/urls.py
+++ atodo/atodo/urls.py
@@ -8,7 +8,8 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'atodo.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^report/', views.status_report, name='report'),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^index/', include('todolist.urls')),
     url(r'^report/', include('todolist.urls')),
 )
